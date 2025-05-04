738ab92c8317798dc24cf389718052e595acfbb3
md-alamin2 <mdalamin22671@gmail.com>
2025-05-04 12:42:10 +0600

    fixed problem of letest news title showing

--- src/Components/News/News.jsx
+++ src/Components/News/News.jsx
@@ -5,7 +5,7 @@ import EmptyState from "../EmptyState/EmptyState";
 import { AuthContext } from "../../Provider/AuthContext";
 
 const News = () => {
-  const { setTitles} =use(AuthContext)
+  const { setTitles } = use(AuthContext);
   const { id } = useParams();
   const data = useLoaderData();
 
@@ -13,26 +13,36 @@ const News = () => {
 
   useEffect(() => {
     if (id == "0") {
-      return setNewNewses(data);
+      setNewNewses(data);
+      const newsesTitle = data.map((items) => items.title);
+      setTitles(newsesTitle);
+      return;
     } else if (id == "1") {
       const newses = data.filter((news) => news.others.is_today_pick === true);
-      return setNewNewses(newses);
+      setNewNewses(newses);
+      const newsesTitle = data
+        .filter((news) => news.others.is_today_pick === true)
+        .map((items) => items.title);
+      setTitles(newsesTitle);
+      return;
     }
     const newses = data.filter((news) => news.category_id == id);
     setNewNewses(newses);
-    const newsesTitle = data.filter(news=>news.category_id == id).map(items=>items.title)
-    setTitles(newsesTitle)
+    const newsesTitle = data
+      .filter((news) => news.category_id == id)
+      .map((items) => items.title);
+    setTitles(newsesTitle);
   }, [id, data, setTitles]);
 
-  if(newNewses.length<1) return<EmptyState></EmptyState>
+  if (newNewses.length < 1) return <EmptyState></EmptyState>;
   return (
     <div className="h-[1725px] overflow-y-auto">
       <p className="text-xl font-semibold ml-6 mb-3">Dragon News</p>
 
       <div className="grid grid-cols-1 gap-5">
-        {
-            newNewses.map(news=><NewsCard key={news.id} news={news}></NewsCard>)
-        }
+        {newNewses.map((news) => (
+          <NewsCard key={news.id} news={news}></NewsCard>
+        ))}
       </div>
     </div>
   );
